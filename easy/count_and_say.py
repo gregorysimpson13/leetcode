b8efb78c04bd7d1c9a332aa66d29ae409a1997bf
Gregory Simpson <gregorysimpson13@gmail.com>
2020-06-15 12:52:20 -0400

    easy count and say problem

---
+++ easy/count_and_say.py
@@ -0,0 +1,54 @@
+# 38. Count and Say - EASY
+# https://leetcode.com/problems/count-and-say/
+# The count-and-say sequence is the sequence of integers with the first five terms as following:
+
+# 1.     1
+# 2.     11
+# 3.     21
+# 4.     1211
+# 5.     111221
+# 1 is read off as "one 1" or 11.
+# 11 is read off as "two 1s" or 21.
+# 21 is read off as "one 2, then one 1" or 1211.
+
+# Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+# Note: Each term of the sequence of integers will be represented as a string.
+
+ 
+
+# Example 1:
+
+# Input: 1
+# Output: "1"
+# Explanation: This is the base case.
+# Example 2:
+
+# Input: 4
+# Output: "1211"
+# Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
+
+
+def countAndSay(n):
+    result = "1"
+    for _ in range(1,n):
+        curr_num = result[0]
+        localResult = ""
+        count = 1
+        for i in range(1, len(result)):
+            if result[i-1] != result[i]:
+                localResult += str(count) + str(curr_num)
+                curr_num = result[i]
+                count = 1
+            else:
+                count = count + 1
+        localResult += str(count) + str(curr_num)
+        result = localResult if len(localResult) > 0 else result
+    return result
+            
+
+
+
+for i in range(1,11):
+    print('{}: {}'.format(i, countAndSay(i)))
+
