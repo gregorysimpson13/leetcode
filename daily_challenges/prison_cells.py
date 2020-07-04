0f05db96e2a658dc5b05a85433b933e44572f1dd
Gregory Simpson <gregorysimpson13@gmail.com>
2020-07-03 22:21:10 -0400

    still not finished

--- daily_challenges/prison_cells.py
+++ daily_challenges/prison_cells.py
@@ -1,12 +1,34 @@
- def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
-        if not cells:
-            return cells
+def prisonAfterNDays(cells, N: int):
+    mod = N % 14
+    for _ in range(mod):
+        prev_value = cells[0]
         cells[0] = 0
-        for _ in range(N):
-            prev_value = 0
-            for i in range(1, len(cells)-1):
-                tmp_value = cells[i]
-                cells[i] = 1 if prev_value == cells[i+1] else 0
-                prev_value = tmp_value
-            cells[-1] = 0
-        return cells
+        for i in range(1, len(cells)-1):
+            tmp_value = cells[i]
+            cells[i] = 1 if prev_value == cells[i+1] else 0
+            prev_value = tmp_value
+        cells[-1] = 0
+    return cells
+
+# print(prisonAfterNDays([0,1,0,1,1,0,0,1], 6))
+# print(prisonAfterNDays([0,1,0,1,1,0,0,1], 454))
+# print(prisonAfterNDays([1,0,0,1,0,0,1,0], 454))
+# print(prisonAfterNDays([1,0,0,1,0,0,1,0], 6))
+# print(prisonAfterNDays([1,0,0,1,0,0,1,0], 1000000000))
+# print(prisonAfterNDays([0,0,0,1,0,0,1,0], 9999999999))
+# print(prisonAfterNDays([0,0,0,1,0,0,1,0], 5))
+# print(prisonAfterNDays([1,0,0,1,0,0,1,0], 1000000000))
+# print(prisonAfterNDays([1,0,0,1,0,0,1,0], 1000000014))
+print(prisonAfterNDays([1,0,0,1,0,0,1,0], 0))
+print(prisonAfterNDays([1,0,0,1,0,0,1,0], 14))
+print(prisonAfterNDays([1,0,0,1,0,0,1,0], 28))
+print(prisonAfterNDays([1,0,0,1,0,0,1,0], 29))
+print(prisonAfterNDays([1,0,0,1,0,0,1,0], 1000000000))
+print(prisonAfterNDays([1,0,0,1,0,0,0,1], 826))
+
+
+
+# [0,0,1,1,1,1,1,0] - 100M
+
+# 
+# [0,1,1,0,1,1,1,0]
