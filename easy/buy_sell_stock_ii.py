a761b7f1b2c676ea3fafb7ea66815960a85f5f8e
Gregory Simpson <gregorysimpson13@gmail.com>
2020-07-13 13:34:37 -0400

    buy sell stock updates

--- easy/buy_sell_stock_ii.py
+++ easy/buy_sell_stock_ii.py
@@ -38,5 +38,13 @@ def maxProfit(prices) -> int:
             current_buy = price
     return profits
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        price = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                price = price + (prices[i] - prices[i-1])
+        return price
+
 print(maxProfit([7,1,5,3,6,4]), 7)
 print(maxProfit([1,2,3,4,5]), 4)
