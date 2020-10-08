e315cd998c46fa803d1219944b7e31cd8bcc6d07
Gregory Simpson <gregorysimpson13@gmail.com>
2020-10-08 08:15:22 -0400

    binary search problem

---
+++ daily_challenges/binary_search.py
@@ -0,0 +1,11 @@
+# Runtime: O(log n)
+# Space: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l+r) // 2
+            if nums[m] == target: return m
+            if nums[m] > target: r = m - 1
+            if nums[m] < target: l = m + 1
+        return -1
